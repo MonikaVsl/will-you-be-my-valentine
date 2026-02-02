a03f8c7ffd27e5f2023105511fb378e64a7f53c6
MonikaVsl <monikavasilevska2005@gmail.com>
2026-02-03 00:19:49 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // <-- ADD THIS LINE
+      base: '/will-you-be-my-valentine/',
+
       server: {
         port: 3000,
         host: '0.0.0.0',
@@ -21,3 +24,4 @@ export default defineConfig(({ mode }) => {
       }
     };
 });
+
